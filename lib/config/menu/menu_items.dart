a5aabb3d1024cd878523e1a91eda0274aa17ebe1
Alessandro Hidaldo <alessandrohidalgo8@gmail.com>
2023-05-16 14:11:35 -0400

    item del menu

---
+++ lib/config/menu/menu_items.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class MenuItems {
+  final String title;
+  final String subtitle;
+  final String link;
+  final IconData icon;
+
+  MenuItems(this.title, this.subtitle, this.link, this.icon);
+}
