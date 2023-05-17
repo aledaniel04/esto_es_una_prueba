0e44b45ac81d189f39413ff2109e1f0558c4b984
Alessandro Hidaldo <alessandrohidalgo8@gmail.com>
2023-05-16 23:32:21 -0400

    agregamos las opciones al home

--- lib/presentation/screen/home/pruba_screen.dart
+++ lib/presentation/screen/home/pruba_screen.dart
@@ -1,3 +1,4 @@
+import 'package:esto_es_una_prueba/config/menu/menu_items.dart';
 import 'package:flutter/material.dart';
 
 class PruebaScreen extends StatelessWidget {
@@ -6,8 +7,48 @@ class PruebaScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: const Text("prueba"), centerTitle: true),
-      body: const Center(child: Text("hola mundo, soy programador")),
+        appBar: AppBar(title: const Text("prueba"), centerTitle: true),
+        body: const _homeView());
+  }
+}
+
+class _homeView extends StatelessWidget {
+  const _homeView();
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView.builder(
+        itemCount: appMenuItems.length,
+        itemBuilder: ((context, index) {
+          final menuItem = appMenuItems[index];
+          return _CustomListTile(menuItem: menuItem);
+        }));
+  }
+}
+
+class _CustomListTile extends StatelessWidget {
+  const _CustomListTile({
+    super.key,
+    required this.menuItem,
+  });
+
+  final MenuItem menuItem;
+
+  @override
+  Widget build(BuildContext context) {
+    return ListTile(
+      tileColor: Colors.blueGrey,
+      leading: Icon(
+        menuItem.icon,
+        color: Colors.indigo,
+      ),
+      trailing: const Icon(
+        Icons.arrow_forward_ios_outlined,
+        color: Colors.indigo,
+      ),
+      title: Text(menuItem.title),
+      subtitle: Text(menuItem.subtitle),
+      onTap: () {},
     );
   }
 }
