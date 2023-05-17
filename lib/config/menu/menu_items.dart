fe6c7caa6e0af1eed8dbd1e2ff90a23794d08665
Alessandro Hidaldo <alessandrohidalgo8@gmail.com>
2023-05-16 22:18:33 -0400

    agregamos las opciones del menu

--- lib/config/menu/menu_items.dart
+++ lib/config/menu/menu_items.dart
@@ -1,10 +1,22 @@
 import 'package:flutter/material.dart';
 
-class MenuItems {
+class MenuItem {
   final String title;
   final String subtitle;
   final String link;
   final IconData icon;
 
-  MenuItems(this.title, this.subtitle, this.link, this.icon);
+  const MenuItem(
+      {required this.title,
+      required this.subtitle,
+      required this.link,
+      required this.icon});
 }
+
+const appMenuItems = <MenuItem>[
+  MenuItem(
+      title: "primer SQLite",
+      subtitle: "ensayo de como usarlo",
+      link: "/SQLite",
+      icon: Icons.data_array_outlined)
+];
